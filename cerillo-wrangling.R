162cba3fd738e982120520959d8374f5b16cd8b3
cbaughan <55101712+cbaughan@users.noreply.github.com>
2023-03-16 16:09:01 -0700

    Add files via upload

---
+++ cerillo-wrangling.R
@@ -0,0 +1,30 @@
+# Christina Guerrero
+# christinagb@arizona.edu
+message("Loading functions from cerillo-wrangling.R")
+# Function for wrangling Cerillo data to input into Growthcurver
+cerillo_wrangling<-function(d){
+  # Remove Temperature Column
+  d<-d[,-(1:3)] 
+  d<-d[,-2]
+  # Convert class to numeric
+  d<-as.data.frame(sapply(d, as.numeric))
+  
+  # Rename column as time
+  names(d)[1] <- "time"
+  d$time <- as.numeric(unlist(d$time))
+  
+  # Convert Linux Time to Time Since Beginning of Experiment in Hours
+  d$time<- d$time - d[1,1] #Subtracting the beginning time point from every value
+  d$time<- d$time/60 # Converting to minute
+  d$time<- d$time/60 # Converting to hours
+  
+  
+  #Convert Columns from A1,A2,A3, to A1,B1, C1
+  d<-melt(d, id=c("time")) 
+  d<- d %>% separate(variable, into = c("text", "num"), 
+                     sep = "(?<=[A-Za-z])(?=[0-9])" )
+  d<-d[order(as.numeric(d$num)),]
+  d<-d %>%unite('variable', text:num, sep = "")
+  d<-pivot_wider(d, names_from= variable, values_from = value)
+  return(d)
+}
